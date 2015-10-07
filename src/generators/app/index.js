f59a5698763254bda5ab76fbf42480e4a7c3a607
Mehdy Dara <mdara@eleven-labs.com>
2015-10-07 11:29:33 +0200

    Clean && use Object.assign instead of deep-extend

--- src/generators/app/index.js
+++ src/generators/app/index.js
@@ -1,4 +1,5 @@
-import extend from 'deep-extend';
+import 'babel/polyfill';
+import path from 'path';
 import { Base } from 'yeoman-generator';
 
 export default class GeneratorYeomanBoilerplateEs6 extends Base {
@@ -6,178 +7,54 @@ export default class GeneratorYeomanBoilerplateEs6 extends Base {
   constructor(...args) {
     super(...args);
 
-    this.option('cssPreprocessor', {
+    this.argument('appName', {
       type: String,
-      required: true
-    });
-
-    this.option('jsPreprocessor', {
-      type: String,
-      required: true
-    });
-
-    this.option('htmlPreprocessor', {
-      type: String,
-      required: true
+      defaults: path.basename(process.cwd())
     });
   }
 
   initializing() {
-    // Pre set the default props from the information we have at this point
-    this.props = {
-      cssPreprocessor: this.options.cssPreprocessor,
-      jsPreprocessor: this.options.jsPreprocessor,
-      htmlPreprocessor: this.options.htmlPreprocessor
+    this.props = {};
+  }
+
+  get prompting() {
+
+    return {
+
+      appName() {
+
+        let done = this.async();
+
+        let prompts = [{
+          type: 'input',
+          name: 'applicationName',
+          message: 'What\'s application name?',
+          default: this.appName
+        }];
+
+        this.prompt(prompts, function (props) {
+          Object.assign(this.props, props);
+
+          done();
+        }.bind(this));
+      }
     };
   }
 
-  prompting() {
-    let done = this.async();
+  writing() {
+    // Write your files
+  }
 
-    let prompts = [{
-      when: !this.props.cssPreprocessor,
-      type: 'list',
-      name: 'cssPreprocessor',
-      message: 'Which CSS preprocessor do you want?',
-      choices: [
-        {
-          name: 'SASS',
-          value: 'sass'
-        }
-      ]
-    }, {
-      when: !this.props.jsPreprocessor,
-      type: 'list',
-      name: 'jsPreprocessor',
-      message: 'Which JS preprocessor do you want?',
-      choices: [
-        {
-          name: 'HTML',
-          value: 'html'
-        }
-      ]
-    }, {
-      when: !this.props.htmlPreprocessor,
-      type: 'list',
-      name: 'htmlPreprocessor',
-      message: 'Which HTML template engine would you want?',
-      choices: [
-        {
-          name: 'JS',
-          value: 'js'
-        }
-      ]
-    }];
-
-    this.prompt(prompts, function (props) {
-      this.props = extend(this.props, props);
-
-      done();
-    }.bind(this));
+  default() {
+    // Compose here with others Yeoman generator
   }
 
   installing() {
-    this.npmInstall();
+    // Install dependencies
   }
 
+  end() {
+    // End message
+    this.log('End of generator-yeoman-boilerplate-es6!');
+  }
 }
-
-//
-// module.exports = generators.Base.extend({
-//   constructor: function () {
-//     generators.Base.apply(this, arguments);
-//
-//     this.option('cssPreprocessor', {
-//       type: String,
-//       required: true
-//     });
-//
-//     this.option('jsPreprocessor', {
-//       type: String,
-//       required: true
-//     });
-//
-//     this.option('htmlPreprocessor', {
-//       type: String,
-//       required: true
-//     });
-//   },
-//
-//   initializing: function () {
-//     // Pre set the default props from the information we have at this point
-//     this.props = {
-//       cssPreprocessor: this.options.cssPreprocessor,
-//       jsPreprocessor: this.options.jsPreprocessor,
-//       htmlPreprocessor: this.options.htmlPreprocessor
-//     };
-//   },
-//
-//   prompting: {
-//     askFor: function () {
-//       var done = this.async();
-//
-//       var prompts = [{
-//         when: !this.props.cssPreprocessor,
-//         type: 'list',
-//         name: 'cssPreprocessor',
-//         message: 'Which CSS preprocessor do you want?',
-//         choices: [
-//           {
-//             name: 'SASS',
-//             value: 'sass'
-//           }
-//         ]
-//       }, {
-//         when: !this.props.jsPreprocessor,
-//         type: 'list',
-//         name: 'jsPreprocessor',
-//         message: 'Which JS preprocessor do you want?',
-//         choices: [
-//           {
-//             name: 'HTML',
-//             value: 'html'
-//           }
-//         ]
-//       }, {
-//         when: !this.props.htmlPreprocessor,
-//         type: 'list',
-//         name: 'htmlPreprocessor',
-//         message: 'Which HTML template engine would you want?',
-//         choices: [
-//           {
-//             name: 'JS',
-//             value: 'js'
-//           }
-//         ]
-//       }];
-//
-//       this.prompt(prompts, function (props) {
-//         this.props = extend(this.props, props);
-//
-//         done();
-//       }.bind(this));
-//     }
-//   },
-//
-//   writing: function () {
-//     // Re-read the content at this point because a composed generator might modify it.
-//
-//     // Let's extend package.json so we're not overwriting user previous fields
-//   },
-//
-//   default: function () {
-//     // this.composeWith('fountain-gulpfile:gulp', {
-//     //   options: {
-//     //     cssPreprocessor: this.props.cssPreprocessor,
-//     //     jsPreprocessor: this.props.jsPreprocessor,
-//     //     htmlPreprocessor: this.props.authorName
-//     //   }
-//     // }, {
-//     //   local: require.resolve('../gulp')
-//     // });
-//   },
-//
-//   installing: function () {
-//     this.npmInstall();
-//   }
-// });
